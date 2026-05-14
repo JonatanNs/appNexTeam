b5f23d39c3dead304bb8b2d6600af330d958dded
JonatanNs <nsualu.jonatan@gmail.com>
2026-05-14 20:42:10 +0200

    feat(back) : initial Spring Boot

---
+++ backend/src/test/java/com/connecteam/ConnecteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connecteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
