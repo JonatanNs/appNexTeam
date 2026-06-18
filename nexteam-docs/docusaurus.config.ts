01993cd05bfe546d544b8e4cb2575ea42a6e7ebe
Jonatan Ns <nsualu.jonatan@gmail.com>
2026-06-18 11:50:00 +0200

    feat: update Docusaurus configuration for GitHub Pages deployment

--- nexteam-docs/docusaurus.config.ts
+++ nexteam-docs/docusaurus.config.ts
@@ -14,22 +14,16 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://JonatanNs.github.io',
+  baseUrl: '/appNexTeam/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'JonatanNs', // Usually your GitHub org/user name.
-  projectName: 'NexTeam', // Usually your repo name.
+  organizationName: 'JonatanNs',
+  projectName: 'appNexTeam',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+  
   i18n: {
     defaultLocale: 'fr',
     locales: ['fr'],
